79f3e3a67b420c85bc51326c30e3eaea6704b279
Zhu Mengdan(Daniel) <enterzhu@gmail.com>
2014-06-01 01:15:45 +0800

    fix the key prevent

--- js/main.js
+++ js/main.js
@@ -32,7 +32,15 @@ $(function () {
     checkStatus();
   }
 
+  window.addEventListener('keydown', function(e) {
+      // space and arrow keys
+      if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+          e.preventDefault();
+      }
+  }, false);
+
   $('body').keydown(function (event) {
+
     switch (event.which) {
       case tableHockey.keys.W:
         tableHockey.leftSliderMoveFlag = true;
@@ -54,6 +62,7 @@ $(function () {
         continueRound();
         break;
     }
+    e.preventDefault();
   });
 
   $('body').keyup(function (event) {
